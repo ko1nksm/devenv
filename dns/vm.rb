89b16c17b9787986144b8d12f8ab2cbef26dc208
Koichi Nakashima <koichi@nksm.name>
2015-12-26 18:05:02 +0900

    Add dns

---
+++ dns/vm.rb
@@ -0,0 +1,13 @@
+config.vm.network "private_network", ip: '192.168.33.12'
+
+config.vm.provision "shell", inline: <<-SHELL
+  #{BOOTSTRAP(vmname)}
+  provide 10config
+  provide 20initialize
+  provide 21upgrade
+  provide 22base-packages
+  provide 31docker #{USERNAME}
+  provide 31samba '192.168.33.'
+  provide 41samba-export #{USERNAME} ~#{USERNAME} #{USERNAME}
+  #{COMPLETE}
+SHELL
