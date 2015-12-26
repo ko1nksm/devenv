dcb723f2fb7b8703b2c63ded269e883e2302a617
Koichi Nakashima <koichi@nksm.name>
2015-12-26 18:04:41 +0900

    Remove 40user-config

--- default/vm.rb
+++ default/vm.rb
@@ -9,9 +9,8 @@ config.vm.provision "shell", inline: <<-SHELL
   provide 24build-packages
   provide 30tools
   provide 31git
-  provide 31docker
+  provide 31docker #{USERNAME}
   provide 31samba '192.168.33.'
-  provide 40user-config #{USERNAME}
   provide 41samba-export #{USERNAME} ~#{USERNAME} #{USERNAME}
   #{COMPLETE}
 SHELL
--- docker/vm.rb
+++ docker/vm.rb
@@ -20,9 +20,8 @@ config.vm.provision "shell", inline: <<-SHELL
   provide 21upgrade
   provide 22base-packages
   provide 30tools
-  provide 31docker
+  provide 31docker #{USERNAME}
   provide 31samba '192.168.33.'
-  provide 40user-config #{USERNAME}
   provide 41samba-export #{USERNAME} ~#{USERNAME} #{USERNAME}
   #{COMPLETE}
 SHELL
