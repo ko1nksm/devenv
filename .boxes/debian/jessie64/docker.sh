e53201ccb688ccf28fc6fb5252714112e2c20e1c
Koichi Nakashima <koichi@nksm.name>
2016-01-30 15:09:29 +0900

    debian/jessie64@latest: stability for install docker

--- .boxes/debian/jessie64/docker.sh
+++ .boxes/debian/jessie64/docker.sh
@@ -2,26 +2,15 @@
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
+cat <<DATA > /etc/apt/apt.conf.d/docker
+Acquire::HTTP::Proxy::apt.dockerproject.org "DIRECT";
+DATA
+
 cat <<DATA > /etc/apt/sources.list.d/docker.list
 deb https://apt.dockerproject.org/repo debian-jessie main
 DATA
 
-cat <<'DATA' > /etc/default/docker
-# Docker Upstart and SysVinit configuration file
-
-# Customize location of Docker binary (especially for development testing).
-#DOCKER="/usr/local/bin/docker"
-
-# Use DOCKER_OPTS to modify the daemon startup options.
-#DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"
-DOCKER_OPTS="--storage-driver=devicemapper"
-
-# If you need Docker to use an HTTP proxy, it can also be specified here.
-#export http_proxy="http://127.0.0.1:3128/"
-
-# This is also a handy place to tweak where Docker's temporary files go.
-#export TMPDIR="/mnt/bigdrive/docker-tmp"
-DATA
-
 apt-get -y update
 apt-get -y install docker-engine
+service docker stop
+rm -rf /var/lib/docker/aufs
