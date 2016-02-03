91dd6fa63e32306e4948358e8ebd529d78b4bf2a
Koichi Nakashima <koichi@nksm.name>
2016-02-03 19:03:36 +0900

    debian/jessie64: add bridge-utils package

--- .boxes/debian/jessie64/packages.sh
+++ .boxes/debian/jessie64/packages.sh
@@ -2,11 +2,13 @@
 
 set -e
 
-apt-get install -y zsh bash-completion lv vim vim-doc nano- wget curl dnsutils jq apt-transport-https
+apt-get install -y \
+  zsh bash-completion lv vim vim-doc nano- wget curl dnsutils jq \
+  apt-transport-https bridge-utils
 
-apt-get install -y build-essential automake gettext re2c pkg-config \
-        libxml2-dev libssl-dev libbz2-dev libsqlite3-dev \
-        libpng12-dev libjpeg-dev libmcrypt-dev libtidy-dev \
-        libxslt1-dev libpcre3-dev liblzma-dev unixodbc-dev \
-        libcurl4-openssl-dev libexpat1-dev \
-        libncurses5-dev libncursesw5-dev tk8.6 libreadline5-
+apt-get install -y
+  build-essential automake gettext re2c pkg-config libxml2-dev libssl-dev \
+  libbz2-dev libsqlite3-dev libpng12-dev libjpeg-dev libmcrypt-dev \
+  libtidy-dev libxslt1-dev libpcre3-dev liblzma-dev unixodbc-dev \
+  libcurl4-openssl-dev libexpat1-dev libncurses5-dev libncursesw5-dev \
+  tk8.6 libreadline5-
