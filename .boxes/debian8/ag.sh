04677c83495e4f57beeb2f7f97fbca73f3232c7a
Koichi Nakashima <koichi@nksm.name>
2016-04-09 03:31:10 +0900

    debian8: refactor

--- .boxes/debian8/ag.sh
+++ .boxes/debian8/ag.sh
@@ -8,8 +8,3 @@ fi
 cd /usr/local/src/the_silver_searcher-${AG_VERSION}
 ./build.sh
 make install
-
-# https://github.com/peco/peco/releases/
-PECO_VERSION=v0.3.5
-curl -sSL https://github.com/peco/peco/releases/download/$PECO_VERSION/peco_linux_amd64.tar.gz | tar zx -C /usr/local/src
-install -m 755 /usr/local/src/peco_linux_amd64/peco /usr/local/bin
---
+++ .boxes/debian8/peco.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# https://github.com/peco/peco/releases/
+PECO_VERSION=v0.3.5
+curl -sSL https://github.com/peco/peco/releases/download/$PECO_VERSION/peco_linux_amd64.tar.gz | tar zx -C /usr/local/src
+install -m 755 /usr/local/src/peco_linux_amd64/peco /usr/local/bin
