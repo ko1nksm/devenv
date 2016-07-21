233ccef21239bb2a6ff1e68cd21a3d558551ecbb
Koichi Nakashima <koichi@nksm.name>
2016-07-21 14:00:35 +0900

    a: upgrade to 0.32.0

--- .boxes/debian8/ag.sh
+++ .boxes/debian8/ag.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # https://github.com/ggreer/the_silver_searcher/releases
-AG_VERSION=0.31.0
+AG_VERSION=0.32.0
 if [ ! -e /usr/local/src/the_silver_searcher-${AG_VERSION} ]; then
     curl -sSL https://github.com/ggreer/the_silver_searcher/archive/${AG_VERSION}.tar.gz | tar zx -C /usr/local/src
 fi
